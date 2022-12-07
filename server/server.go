ef2a8a7dfadc45470a6caa0962e6bb3f910a609b
Akhmad-S <akhmadsuyunov95@gmail.com>
2022-12-07 15:41:22 +0500

    grpc init

---
+++ server/server.go
@@ -0,0 +1,34 @@
+package tutorial
+
+import (
+	context "context"
+	"grpctutorial/proto-gen/tutorial"
+	"math/rand"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+)
+
+// We define a server struct that implements the server interface.
+type Server struct {
+	tutorial.UnimplementedTutorialServer
+}
+
+// We implement the SayHello method of the server interface.
+func (s *Server) SayHello(ctx context.Context, in *tutorial.HelloRequest) (*tutorial.HelloReply, error) {
+	return &tutorial.HelloReply{
+		Message: "Hello, " + in.GetName()}, nil
+}
+func (s *Server) RollDice(ctx context.Context, req *tutorial.RollDiceRequest) (*tutorial.RollDiceResponse, error) {
+	var res tutorial.RollDiceResponse
+
+	if req.Num < 0{
+		return nil, grpc.Errorf(codes.InvalidArgument, "number should be positive")
+	}
+	
+	for i := 0; i < int(req.Num); i++ {
+		res.Dice = append(res.Dice, int32(rand.Intn(100)))
+	}
+
+	return &res, nil
+}
